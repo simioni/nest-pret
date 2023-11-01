4911e10b9095924725920eab8ca40937181874f2
simioni <rsimioni@gmail.com>
2023-11-01 16:32:09 -0300

    Jest e2e config

---
+++ test/jest-e2e.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  rootDir: '..',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  verbose: false,
+  silent: false,
+};
+
+export default config;
