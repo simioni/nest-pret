607b503c253e076f5b33d0df26cc9accffb0d5e0
simioni <rsimioni@gmail.com>
2023-11-01 21:53:58 -0300

    Removed all global providers from useGlobal in favor of importing them in the modules so they are carried into tests automatically.

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
 
   /**********************************
    *
