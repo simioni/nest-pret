d498de70c2bcfc668965dd23b1ab98600c1d67b6
simioni <rsimioni@gmail.com>
2023-11-20 03:58:06 -0300

    Streamlined dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,7 +2,7 @@ import { IntersectionType, PartialType, PickType } from '@nestjs/swagger';
 import { User } from '../schemas/user.schema';
 
 const requiredFields = ['email', 'password'] as const;
-const optionalFields = ['name', 'surname', 'phone', 'birthdaydate'] as const;
+const optionalFields = ['name', 'familyName', 'phone', 'birthDate'] as const;
 
 export class CreateUserDto extends IntersectionType(
   PartialType(PickType(User, optionalFields)),
