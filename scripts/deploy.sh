33a140923c4b3cafff586390ffd14c687f84a3bd
simioni <rsimioni@gmail.com>
2023-11-24 13:02:46 -0300

    tests

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # ANSI escape codes for colored logs. For more colors, see: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-Color_Off='\033[0m'       # Text Reset
-Yellow='\033[0;33m'       # Yellow
-Green='\033[0;32m'        # Light Green
-Blue='\033[0;34m'         # Light Blue
-Purple='\033[0;35m'       # Purple
-BRed='\033[1;31m'         # Bold Red
-BGreen='\033[1;32m'       # Bold Green
-BPurple='\033[1;35m'      # Bold Purple
-BBlue='\033[1;34m'        # Bold Blue
-UGreen='\033[4;32m'       # Underline Green
-DarkGray="\033[1;30m"     # Dark gray
+Color_Off='\033[0m'         # Text Reset
+Bold='\033[1m'              # Bold
+Black='\033[0;30m'          # Black
+Yellow='\033[0;33m'         # Yellow
+Red='\033[31m'              # Red
+Green='\033[0;32m'          # Light Green
+Blue='\033[0;34m'           # Light Blue
+Purple='\033[0;35m'         # Purple
+BRed='\033[1;31m'           # Bold Red
+BGreen='\033[1;32m'         # Bold Green
+BPurple='\033[1;35m'        # Bold Purple
+BBlue='\033[1;34m'          # Bold Blue
+UGreen='\033[4;32m'         # Underline Green
+URed='\033[4;31m'           # Underline Red
+DarkGray="\033[1;30m"       # Dark gray
+Flag_Green='\033[42;30;4m'  # Black on Green underlined
+Flag_Red='\033[101;30;4m'   # Black on Red underlined
 
 
 # makes sure the working directory is clean
@@ -20,16 +26,20 @@ if git diff-index --quiet HEAD
 then
   GIT_MODS="clean"
 else
-  printf "${BRed}[ERROR] ${Color_Off}Working directory is not clean!\n" >&2
-  printf "${Color_Off}Make sure to ${UGreen}commit any changes${Color_Off} before pushing a new deployment.\n\n" >&2
+  printf " \e${Flag_Red}[ERROR]${Color_Off}${Bold} Working directory is not clean!\n\n" >&2
+  printf "${Color_Off} Make sure to ${Red}commit any changes${Color_Off} before pushing a new deployment.\n\n" >&2
   exit 1
 fi
 echo $GIT_MODS
 
 # run tests
 printf "\n${Purple}[TESTING]${Color_Off} Running unit tests...\n"
+npm run test
+
 printf "\n${Purple}[TESTING]${Color_Off} Running end-to-end tests...\n"
-printf "\n${Green} All tests passed!\n"
+npm run e2e
+
+printf "\n${Green}✅ All tests passed!\n"
 
 # bump the package version
 printf "\n${Purple}[VERSIONING]${Color_Off} Bumping the app version...\n"
@@ -119,7 +129,7 @@ printDeployFinishedBox() {
   FULL_TEXT_WIDTH=$(($(wc -L <<< "$FULL_TEXT")))
 
   SECOND_LINE_COLORED="${Color_Off}${SECOND_LINE}"
-  FULL_TEXT_COLORED="${Color_Off}${ICON}${BGreen}${LABEL}${Color_Off}${PRETEXT}${BGreen}${VERSION}${Color_Off}${MIDTEXT}${Blue}${POSTTEXT}${BGreen}"
+  FULL_TEXT_COLORED="${Color_Off}${ICON}${Flag_Green}${LABEL}${Color_Off}${PRETEXT}${BGreen}${VERSION}${Color_Off}${MIDTEXT}${Blue}${POSTTEXT}${BGreen}"
 
   printBlockTop $(replaceByDashes "$(centerText "" "$FULL_TEXT_WIDTH")")
   printBlockRow "$(centerText "" "$FULL_TEXT_WIDTH")"
